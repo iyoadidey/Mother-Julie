a171b930ed7227025c0e9bf39935517068ab46e4
iyoadidey <cabrerajiro@gmail.com>
2025-10-07 22:34:23 +0800

    Add Item model CRUD: models, admin, views, urls, templates; migrations applied

--- mysite/hello/models.py
+++ mysite/hello/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
 # Create your models here.
--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Item
 
 def hello_world(request):
     return HttpResponse("Hello, world! This is your first Django view.")
@@ -15,3 +18,34 @@ def signup_view(request):
 
 def terms_view(request):
     return render(request, 'Terms&Conditions.htm')
+
+
+class ItemListView(ListView):
+    model = Item
+    template_name = 'items/item_list.html'
+    context_object_name = 'items'
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'items/item_detail.html'
+
+
+class ItemCreateView(CreateView):
+    model = Item
+    fields = ['name', 'description']
+    template_name = 'items/item_form.html'
+    success_url = reverse_lazy('item-list')
+
+
+class ItemUpdateView(UpdateView):
+    model = Item
+    fields = ['name', 'description']
+    template_name = 'items/item_form.html'
+    success_url = reverse_lazy('item-list')
+
+
+class ItemDeleteView(DeleteView):
+    model = Item
+    template_name = 'items/item_confirm_delete.html'
+    success_url = reverse_lazy('item-list')
