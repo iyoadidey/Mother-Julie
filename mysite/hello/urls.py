750993a9fed75e08710539d0572b273f4792743b
iyoadidey <cabrerajiro@gmail.com>
2025-10-07 22:19:32 +0800

    Configure templates/static; add views and urls; convert templates to Django tags

--- mysite/hello/urls.py
+++ mysite/hello/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.hello_world, name='hello_world'),
+    path('signin/', views.signin_view, name='signin'),
+    path('signup/', views.signup_view, name='signup'),
+    path('terms/', views.terms_view, name='terms'),
 ]
--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def hello_world(request):
     return HttpResponse("Hello, world! This is your first Django view.")
+
+
+def signin_view(request):
+    return render(request, 'signin.htm')
+
+
+def signup_view(request):
+    return render(request, 'signup.htm')
+
+
+def terms_view(request):
+    return render(request, 'Terms&Conditions.htm')
