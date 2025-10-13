29a589043177b15bae792c04cbc7129daa4dab57
iyoadidey <cabrerajiro@gmail.com>
2025-10-14 00:41:59 +0800

    Initialized

---
+++ mysite/hello/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class SignUpForm(forms.ModelForm):
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
+    
+    # Add first name and last name fields
+    first_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=30, required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']  # Include the new fields here
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords do not match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the user with the hashed password
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])  # Set the password
+        if commit:
+            user.save()
+        return user
