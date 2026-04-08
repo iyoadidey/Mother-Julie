cb69595412471b5ab2396456405a01684a5d3b12
Jiro Adrich Cabrera <cabrerajiro@gmail.com>
2026-04-09 03:32:33 +0800

    Fix Render Django startup path

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+PROJECT_DIR="$SCRIPT_DIR/mysite"
+
+pip install -r "$SCRIPT_DIR/requirements.txt"
+cd "$PROJECT_DIR"
 python manage.py collectstatic --no-input
 python manage.py migrate
